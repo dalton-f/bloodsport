f86c29a318b3e93e2eec42f904d41114668d766e
Dalton <daltonfiggdavies@gmail.com>
2025-10-04 16:04:32 +0100

    Rename WaveManager

--- Entities/WaveManager/WaveManager.gd
+++ Entities/WaveManager/WaveManager.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @export var spawn_points: Array[NodePath] 
 @export var player: CharacterBody3D
+@export var spawn_sfx: AudioStream
+
 
 var waves = [
 	{
@@ -29,6 +31,7 @@ var waves = [
 
 var current_wave := 0
 var enemies_alive := 0
+var spawned_enemies: Array = []
 
 signal wave_started(wave_index)
 signal wave_completed(wave_index)
@@ -36,10 +39,18 @@ signal all_waves_completed()
 
 func _ready() -> void:
 	start_waves()
-	
 
-func start_waves():
+func reset():
+	for enemy in spawned_enemies:
+		if is_instance_valid(enemy):
+			enemy.queue_free()
+	spawned_enemies.clear()
+
 	current_wave = 0
+	enemies_alive = 0
+
+func start_waves():	
+	reset()
 	_start_wave()
 
 func _start_wave():
@@ -51,6 +62,7 @@ func _start_wave():
 	emit_signal("wave_started", current_wave)
 	
 	player.show_wave_number(current_wave)
+	await get_tree().create_timer(1,5).timeout
 
 	spawn_wave(wave)
 
@@ -62,18 +74,25 @@ func spawn_wave(wave):
 			
 func spawn_enemy(enemy_scene: PackedScene):
 	var enemy = enemy_scene.instantiate()
-	var spawn_point = get_node(spawn_points[randi() % spawn_points.size()])
+	var spawn_point = get_node(spawn_points.pick_random())
 	enemy.player = player
 
 	add_child(enemy)
 		
-	enemy.global_transform.origin = spawn_point.global_transform.origin + Vector3(0, 0.5, 0)
+	enemy.global_transform.origin = spawn_point.global_transform.origin + Vector3(0, 1, 0)
 
 	enemies_alive += 1
 	enemy.health_component.connect("died", Callable(self, "_on_enemy_died"), CONNECT_ONE_SHOT)
 
+	AudioManager.play_sfx(spawn_sfx)
+	
+	player.show_enemies_number(enemies_alive)
+	spawned_enemies.append(enemy)
+	
 func _on_enemy_died():
 	enemies_alive -= 1
+	
+	player.show_enemies_number(enemies_alive)
 	
 	if enemies_alive <= 0:
 		emit_signal("wave_completed", current_wave)
