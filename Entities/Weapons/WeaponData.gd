0a4cbd601e62f0c921bd88d5d3d8947b44864722
Dalton <daltonfiggdavies@gmail.com>
2025-10-05 10:35:05 +0100

    Rename range to avoid conflicts

--- Entities/Weapons/WeaponData.gd
+++ Entities/Weapons/WeaponData.gd
@@ -7,5 +7,5 @@ class_name WeaponData
 @export var fire_rate: float = 3.5
 @export var pellets: int = 1
 @export var bullet_spread: float = 0
-@export var range: int = 100
+@export var weapon_range: int = 100
 @export var sfx: AudioStream
