d12541d5094e00b2defd861b819885f5af930e27
Dalton <daltonfiggdavies@gmail.com>
2025-10-05 10:36:12 +0100

    Fix caps with chosen font

--- Entities/Singletons/EffectsManager.gd
+++ Entities/Singletons/EffectsManager.gd
@@ -2,17 +2,33 @@ extends Node
 
 var pairs = [
 	{
-		"buff": {"name": "Speed Boost", "apply": func(p): p.set_speed_multiplier(1.5), "revert": func(p): p.set_speed_multiplier(1.0)},
-		"debuff": {"name": "Damage Down", "apply": func(p): p.weapon_manager.set_damage_mult(0.5), "revert": func(p): p.weapon_manager.set_damage_mult(1)},
+		"buff": {"name": "SPEED BOOST", "apply": func(p): p.set_speed_multiplier(1.5), "revert": func(p): p.set_speed_multiplier(1.0)},
+		"debuff": {"name": "DAMAGE DOWN", "apply": func(p): p.weapon_manager.set_damage_mult(0.5), "revert": func(p): p.weapon_manager.set_damage_mult(1)},
 	},
 	
 	{
-		"buff": {"name": "Long Range", "apply": func(p): p.weapon_manager.set_range_mult(2), "revert": func(p): p.weapon_manager.set_range_mult(1)},
-		"debuff": {"name": "Slow Down", "apply": func(p): p.set_speed_multiplier(0.75), "revert": func(p): p.set_speed_multiplier(1)},
+		"buff": {"name": "LONG RANGE", "apply": func(p): p.weapon_manager.set_range_mult(2), "revert": func(p): p.weapon_manager.set_range_mult(1)},
+		"debuff": {"name": "SLOW DOWN", "apply": func(p): p.set_speed_multiplier(0.75), "revert": func(p): p.set_speed_multiplier(1)},
 	},
+	
+	{
+		"buff": {"name": "DOUBLE DAMAGE", "apply": func(p): p.weapon_manager.set_damage_mult(2), "revert": func(p): p.weapon_manager.set_damage_mult(1)},
+		"debuff": {"name": "RANGE DOWN", "apply": func(p): p.weapon_manager.set_range_mult(0.75), "revert": func(p): p.weapon_manager.set_range_mult(1)},
+	},
+	
+	{
+		"buff": {"name": "QUICK ATTACKER", "apply": func(p): p.weapon_manager.set_attack_speed_mult(2), "revert": func(p): p.weapon_manager.set_attack_speed_mult(1)},
+		"debuff": {"name": "CLOSE RANGE ONLY", "apply": func(p): p.weapon_manager.set_range_mult(0.5), "revert": func(p): p.weapon_manager.set_range_mult(1)},
+	}
 ]
 
 var extremes = [
-	{"name": "Insane Damage!", "apply": func(p): p.weapon_manager.set_damage_mult(5), "revert": func(p): p.weapon_manager.set_damage_mult(1)},
-	{"name": "Totally Broken Slow", "apply": func(p): p.movement.set_speed_multiplier(0.25), "revert": func(p): p.movement.set_speed_multiplier(1.0)}
+	{"name": "INSANE DAMAGE", "apply": func(p): p.weapon_manager.set_damage_mult(5), "revert": func(p): p.weapon_manager.set_damage_mult(1)},
+	{"name": "TOTALLY BROKEN SLOW", "apply": func(p): p.movement.set_speed_multiplier(0.25), "revert": func(p): p.movement.set_speed_multiplier(1.0)},
+	{"name": "LOOONGER RANGE", "apply": func(p): p.weapon_manager.set_range_mult(4), "revert": func(p): p.weapon_manager.set_range_mult(1)},
+	{"name": "QUICKEST ATTACKER", "apply": func(p): p.weapon_manager.set_attack_speed_mult(3), "revert": func(p): p.weapon_manager.set_attack_speed_mult(1)},
+	{"name": "SLOWEST ATTACKER", "apply": func(p): p.weapon_manager.set_attack_speed_mult(0.5), "revert": func(p): p.weapon_manager.set_attack_speed_mult(1)},
+	{"name": "SHORTEST RANGE", "apply": func(p): p.weapon_manager.set_range_mult(0.5), "revert": func(p): p.weapon_manager.set_range_mult(1)},
+	{"name": "FANTASTICALY FAST", "apply": func(p): p.movement.set_speed_multiplier(3), "revert": func(p): p.movement.set_speed_multiplier(1.0)},
+	{"name": "AWFUL DAMAGE", "apply": func(p): p.weapon_manager.set_damage_mult(0.3), "revert": func(p): p.weapon_manager.set_damage_mult(1)},
 ]
