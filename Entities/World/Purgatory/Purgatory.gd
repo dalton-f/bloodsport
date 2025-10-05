1d6842afefb32f08e8a1812c7f74da6e34240367
Dalton <daltonfiggdavies@gmail.com>
2025-10-05 13:07:39 +0100

    Import button and finalise purgatory

--- Entities/World/Purgatory/Purgatory.gd
+++ Entities/World/Purgatory/Purgatory.gd
@@ -1,17 +1,41 @@
 extends Node3D
 
 @onready var pair_display = $Pair/PairLabel
-@onready var extreme_display = $Extreme
+@onready var extreme_display = $Extreme/ExtremeLabel
+@onready var extreme_area = $Extreme/ExtremeArea3D
+@onready var normal_area = $Pair/NormalArea
+
+var chosen_pair
+var chosen_extreme
+var player
 
 func _ready():
-	randomize()
+	normal_area.body_entered.connect(_on_normal_chosen)
+	extreme_area.body_entered.connect(_on_extreme_chosen)
 	
-	var chosen_pair = EffectsManager.pairs.pick_random()
-	var chosen_extreme = EffectsManager.extremes.pick_random()
-	
-	pair_display.text = chosen_pair["buff"]["name"] + " / " +  chosen_pair["debuff"]["name"]
-	extreme_display.text = chosen_extreme["name"]
+	player =  get_parent().get_node("Player")
 
+func randomize_effects():
+	randomize()
+		
+	chosen_pair = EffectsManager.pairs.pick_random()
+	chosen_extreme = EffectsManager.extremes.pick_random()
+	
+	pair_display.text = chosen_pair["buff"]["name"] + " / " + chosen_pair["debuff"]["name"]
+
+func _on_normal_chosen(body):
+	if not body.is_in_group("player"):
+		return
+	
+	player.apply_normal_pair(chosen_pair)
+	restart_game()
+
+func _on_extreme_chosen(body):
+	if not body.is_in_group("player"):
+		return
+	
+	player.apply_extreme(chosen_extreme)
+	restart_game()
 	
 func restart_game():
 	var root = get_tree().current_scene
